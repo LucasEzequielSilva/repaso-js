71bec69ea221cff83c5703e8c15f45f07ca268f3
mrain00 <luxassilva@gmail.com>
2022-05-02 04:29:15 -0300

    update cards

--- main.js
+++ main.js
@@ -12,14 +12,16 @@ console.log(data);
 
 function mostrarProduct() {
     var products = []
-    if(data === undefined) {
+    if (data === undefined) {
+        products.push(...data)
+        console.log(products)
+    } else {
         products.push(...data)
         console.log(products)
     }
-    products.push(...data)
-    var html
-        products.filter(product=> product.id == 1).map(products =>{ 
-            html+=`
+    let html = ''
+    products.filter(product => product.id == 1).map(products => {
+        html += `
             <div class="first-product container-product">
             <img src='${products.image}' alt="image-product" class="img-product"/>
             <div class="h-p-container">
@@ -28,22 +30,40 @@ function mostrarProduct() {
             </div>
             </div>
             `
-        })
-        function hoverImg(event) {
-            event.style.transform = 'translate3d(0px, 0px, 0px) scale3d(1.03, 1.03, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg)';
-        }
-        document.querySelector('#product').innerHTML = html
-        var imgProduct = document.querySelector('.img-product')
-        imgProduct.addEventListener('mouseenter', ()=>{
-            imgProduct.style.transform = 'translate3d(0px, 0px, 0px) scale3d(1.03, 1.03, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg)';
-            imgProduct.style.transformStyle = 'preserve-3d';
-            imgProduct.style.willChange = 'transform';
-            imgProduct.style.transition = '2s';
-        })
-        imgProduct.addEventListener('mouseleave', ()=>{
-            imgProduct.style.transform = 'translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg)';
-            imgProduct.style.transformStyle = 'preserve-3d';
-        })
-        console.log(imgProduct);
-    }
-   
+    })
+    products.filter(product => product.id === 2).map(products => {
+        html += `
+            <div class="second-product container-product">
+            <img src='${products.image}' alt="image-product" class="img-product"/>
+            <div class="h-p-container">
+            <h2 class='h-product'>${products.name}</h2>
+            <p class="p-product">${products.description}</p>
+            </div>
+            </div>
+            `
+    })
+    products.filter(product => product.id === 3).map(products => {
+        html += `
+            <div class="third-product container-product">
+            <img src='${products.image}' alt="image-product" class="img-product"/>
+            <div class="h-p-container">
+            <h2 class='h-product'>${products.name}</h2>
+            <p class="p-product">${products.description}</p>
+            </div>
+            </div>
+            `
+    })
+    products.filter(product => product.id === 4).map(products => {
+        html += `
+            <div class="fourth-product container-product">
+            <img src='${products.image}' alt="image-product" class="img-product"/>
+            <div class="h-p-container">
+            <h2 class='h-product'>${products.name}</h2>
+            <p class="p-product">${products.description}</p>
+            </div>
+            </div>
+            `
+    })
+    document.querySelector('#product').innerHTML = html
+
+}
